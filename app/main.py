2b94524db89f8697fd3ce79e81366fc0f5a2ad6b
Dima <giomozda74@gmail.com>
2025-05-28 18:00:01 +0300

    solution without comments

--- app/main.py
+++ app/main.py
@@ -26,24 +26,10 @@ class Book:
 
     def display(self, renderer: ContentRenderer) -> None:
         renderer.render(self.content)
-        # if display_type == "console":
-        #     print(self.content)
-        # elif display_type == "reverse":
-        #     print(self.content[::-1])
-        # else:
-        #     raise ValueError(f"Unknown display type: {display_type}")
 
     def print_book(self, printer: ContentRenderer) -> None:
         print(f"Printing the book: {self.title}...")
         printer.render(self.content)
-        # if print_type == "console":
-        #     print(f"Printing the book: {self.title}...")
-        #     print(self.content)
-        # elif print_type == "reverse":
-        #     print(f"Printing the book in reverse: {self.title}...")
-        #     print(self.content[::-1])
-        # else:
-        #     raise ValueError(f"Unknown print type: {print_type}")
 
 
 class Serializer(ABC):
